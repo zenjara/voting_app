8969125966dc6947da6c82a5fc0b5f9a61e604a3
Ivan <ivan.matas2@gmail.com>
2016-11-09 17:31:58 +0000

    complete registration

--- app.js
+++ app.js
@@ -1,15 +1,34 @@
 var express = require("express"),
+    app= express(),
     mongoose = require("mongoose"),
-    bodyParser = require("body-parser")
+    bodyParser = require("body-parser"),
     User        =require("./models/user"),
-    Poll        =require("./models/poll")
+    Poll        =require("./models/poll"),
+    indexRoutes =require("./routes/index"),
+    passport    = require("passport"),
+    LocalStrategy = require("passport-local")
     
-var app= express();
+
 
 mongoose.connect("mongodb://localhost/voting_app");
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.use(require("express-session")({
+    secret: "matas je kralj",
+    resave: false,
+    saveUninitialized: false
+}));
+
+app.use("/",indexRoutes);
+
 
 
 
---
+++ routes/index.js
@@ -0,0 +1,32 @@
+var express = require("express"),
+    router  = express.Router(),
+    bodyParser = require("body-parser"),
+    User        =require("../models/user"),
+    Poll        =require("../models/poll"),
+    passport    =require("passport")
+
+router.get("/", function(req,res){
+    res.render("home");
+});
+router.get("/index",function(req, res) {
+    res.render("index");
+});
+
+// Register routes
+router.get("/register", function(req,res){
+    res.render("register");
+});
+router.post("/register",function(req,res){
+    User.register(new User({name:req.body.name, username: req.body.email}),req.body.password,function(err,registeredUser){
+        if(err){
+            console.log(err);
+        }else{
+            passport.authenticate("local")(req,res,function(){
+                res.redirect("index");
+            });
+            
+        }
+    });
+});
+
+module.exports=router;
